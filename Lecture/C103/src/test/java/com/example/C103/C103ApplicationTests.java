e3acd761724eb83d6269897b90f08eca449a1abe
Jindnjs <145753929+Jindnjs@users.noreply.github.com>
2024-07-03 14:51:10 +0900

    C103v1
    init

---
+++ Lecture/C103/src/test/java/com/example/C103/C103ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.C103;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C103ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
