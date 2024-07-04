f304fcdc0853b4750621837fb289cd68ca72e178
Jindnjs <145753929+Jindnjs@users.noreply.github.com>
2024-07-04 16:37:27 +0900

    C104JEv9
    보드 컨트롤러에서 데이터를 객체형식으로
    
    @ModelAttribute 사용하는 방식으로 코드 변경
    
    게시판 기능 업그레이드
    
    ////@GetMapping("/boarddetail/{no}")
    
    @PathVariable 어노테이션 사용해서
    /boarddetail/숫자
    
    /boarddetail/{no}에서
    에서 숫자를 받아오는 어노테이션임
    
    이를 통해 번호를 변수로 관리 가능.

--- Lecture/C104JavaExam/src/main/java/com/example/C104JavaExam/BoardController.java
+++ Lecture/C104JavaExam/src/main/java/com/example/C104JavaExam/BoardController.java
@@ -2,8 +2,10 @@ package com.example.C104JavaExam;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BoardController {
@@ -12,20 +14,39 @@ public class BoardController {
 	public String board() {
 		return "board";
 	}
+//	
+//	@PostMapping("/board")
+//	public String boardData(@RequestParam ("title") String title,
+//							@RequestParam ("content") String content
+//							) {
+//		
+//		System.out.println(title);
+//		System.out.println(content);
+//		return "redirect:/";
+//		
+//	}
 	
+	//ModelAttribute어노테이션을 사용하여 객체 형식으로 주고받기
+	//원래는 변수 하나하나를 리퀘스트 파람으로 전달받았음
 	@PostMapping("/board")
-	public String boardData(@RequestParam ("title") String title,
-							@RequestParam ("content") String content
-							) {
-		
-		System.out.println(title);
-		System.out.println(content);
-		return "redirect:/";
-		
+	public String board(@ModelAttribute Board board) {
+		System.out.println(board.getTitle());
+		System.out.println(board.getContent());
+		return "redirect:/boardlist";
 	}
 	
-	@GetMapping("/bbd")
-	public String bbd() {
-		return "bbd";
+	@GetMapping("/boardlist")
+	public String boardlist() {
+		return "boardlist";
+	}
+	
+	//보드 기능 업그레이드
+	
+	@GetMapping("/boarddetail/{no}")
+	public String boarddetail(@PathVariable("no") String no) {
+		
+		System.out.println("넘어온 번호는 = "+no);
+		return "redirect:/boardlist";
+		
 	}
 }
