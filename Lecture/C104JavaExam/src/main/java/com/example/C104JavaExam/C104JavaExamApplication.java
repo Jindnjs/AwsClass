8e51428117d19c720ac73f2459a062dca4c3adca
Jindnjs <145753929+Jindnjs@users.noreply.github.com>
2024-07-04 12:39:45 +0900

    C104JEv1
    init

---
+++ Lecture/C104JavaExam/src/main/java/com/example/C104JavaExam/C104JavaExamApplication.java
@@ -0,0 +1,13 @@
+package com.example.C104JavaExam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C104JavaExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C104JavaExamApplication.class, args);
+	}
+
+}
---
+++ Lecture/C104JavaExam/src/test/java/com/example/C104JavaExam/C104JavaExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.C104JavaExam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C104JavaExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
