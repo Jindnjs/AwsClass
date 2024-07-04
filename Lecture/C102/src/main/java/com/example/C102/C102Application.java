a5eb2b07f47b281e08754c692bf9c28ea4611317
Jindnjs <145753929+Jindnjs@users.noreply.github.com>
2024-07-04 15:51:18 +0900

    C102v1
    2일차 css실습 내용

---
+++ Lecture/C102/src/main/java/com/example/C102/C102Application.java
@@ -0,0 +1,13 @@
+package com.example.C102;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C102Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C102Application.class, args);
+	}
+
+}
---
+++ Lecture/C102/src/test/java/com/example/C102/C102ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.C102;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C102ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
