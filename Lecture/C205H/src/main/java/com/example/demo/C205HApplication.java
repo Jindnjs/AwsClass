a8822ef76a147b98be0e32ee5963b6ce7a48959b
Jindnjs <145753929+Jindnjs@users.noreply.github.com>
2024-07-12 15:24:47 +0900

    아마존 인스턴스에 올리기

---
+++ Lecture/C205H/src/main/java/com/example/demo/C205HApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C205HApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C205HApplication.class, args);
+	}
+
+}
