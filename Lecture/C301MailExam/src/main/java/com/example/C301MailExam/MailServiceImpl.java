de9a6d13637a221402d4b97b1d2e30c1780db90f
Jindnjs <145753929+Jindnjs@users.noreply.github.com>
2024-07-15 14:18:47 +0900

    v2
    메일 발송 구현완료

--- Lecture/C301MailExam/src/main/java/com/example/C301MailExam/MailServiceImpl.java
+++ Lecture/C301MailExam/src/main/java/com/example/C301MailExam/MailServiceImpl.java
@@ -1,10 +1,15 @@
 package com.example.C301MailExam;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MailServiceImpl implements MailService {
 
+	@Autowired
+	private JavaMailSender mailsender;
 	@Override
 	public void create(String title, String content,String addr) {
 		// TODO Auto-generated method stub
@@ -13,6 +18,17 @@ public class MailServiceImpl implements MailService {
 		System.out.println("제목 : " + title);
 		System.out.println("내용 : " + content);
 		System.out.println("주소 : " + addr);
+		
+		SimpleMailMessage message = new SimpleMailMessage();
+		
+		message.setFrom("yoojinwon@naver.com");
+		message.setTo(addr);
+		
+		message.setSubject(title);
+		message.setText(content);
+		
+		mailsender.send(message);
 	}
+	
 
 }
